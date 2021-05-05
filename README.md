59c505bc46848c7d95a0cca6599957671d2095de
Kingsman44 <shivan.0972@gmail.com>
2021-05-05 18:00:56 +0530

    add more functions

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Generate Fixes for your SELinux Denials.
 Generates Fixes at sepolicy/vendor/*.te files
 
 ## Comands
-`. sepolicy.sh {File-Name} {options}`
+`. sepolicy.sh File1 File2 .. {options}`
 
 **Options:-** <br>
-`--clean` To remove old sepolicy denials and generate new at sepolicy/vendor/*.te
+`--clean`                    : Removes old sepolicy and start with clean
+`-s scontext1,scontext2..`   : Only resolve denials for given scontexts
+`-r scontext1,scontext2..`   : Ignores denials for given scontexts
+
+Use , for multiple scontexts in -r and -s
+Example `-s init,zygote`
--- sepolicy.sh
+++ sepolicy.sh
@@ -1,6 +1,32 @@
 sepolicy() {
 scontext=$(grep "avc:" $1 | grep "denied" | sed -n -e 's/^.*scontext=//p' | cut -d: -f3 | sort  | uniq)
+
 for i in $scontext; do
+  scon_check=0
+  rcheck=0
+  if [ ! -z "$scon" ]; then
+    for j in $scon; do
+      if [ $j == $i ]; then
+	scon_check=1
+	break
+      fi
+    done
+  elif [ ! -z "$rcon" ]; then
+    for j in $rcon; do
+      if [ $j == $i ]; then
+	rcheck=1
+	break
+      fi
+    done
+    if [ $rcheck -eq 0 ]; then
+      scon_check=1
+    else
+      scon_check=0
+    fi
+  fi
+  if [ $scon_check -eq 0 ] && ([ ! -z "$scon" ] || [ ! -z "$rcon" ]); then
+  continue
+  fi
   permission=''
   tclass=$(grep $i $1 | sed -n -e 's/^.*tclass=//p' | cut -d" " -f1 | sort | uniq)
   for d in $tclass; do
@@ -64,20 +90,86 @@ for i in $scontext; do
 done
 }
 
+error=0
+scon=""
+rcon=""
+
 if [ ! -d sepolicy ]; then
 mkdir sepolicy
 fi
-
 if [ ! -d sepolicy/vendor ]; then
 mkdir sepolicy/vendor
 fi
 
-for opt in $@; do
-if [ $opt == "--clean" ]; then
-echo "Cleaning old sepolicy"
+echo "-"
+echo "Sepolicy Fixer v1"
+echo "by Kingsman44"
+echo "-"
+echo ""
+
+# Options
+opt=$@
+if [ ! -z "$(echo "$opt" | grep -e '--help')" ]; then
+echo "Usage: "
+echo ". sepolicy.sh file1 file2 ... { options } "
+echo ""
+echo "Options"
+echo "--clean                    : Removes old sepolicy and start with clean"
+echo "-s scontext1,scontext2..   : Only resolve denials for given scontexts"
+echo "-r scontext1,scontext2..   : Ignores denials for given scontexts"
+echo ""
+echo "Use , for multiple scontexts in -r and -s"
+echo "Example: -s init,zygote"
+error=1
+fi
+
+if [ ! -z "$(echo "$opt" | grep -e '-s')" ] && [ $error -eq 0 ]; then
+scon="$(echo "$opt" | sed -n -e 's/^.*-s //p' | cut -d' ' -f1)"
+scon="$(echo $scon | tr "," " ")"
+if [ -z "$scon" ] || [ "${scon:0:1}" == "-" ]; then
+echo "Error:"
+echo "-s option used but no scontext Defined."
+echo "use --help for more information"
+error=1
+else
+echo "-s option used"
+echo "only fixes for $scon will be generated."
+echo ""
+fi
+fi
+
+if [ ! -z "$(echo "$opt" | grep -e '-r')" ] && [ $error -eq 0 ]; then
+rcon="$(echo "$opt" | sed -n -e 's/^.*-r //p' | cut -d' ' -f1)"
+rcon="$(echo $rcon | tr "," " ")"
+if [ -z "$rcon" ] || [ "${rcon:0:1}" == "-" ]; then
+echo "Error:"
+echo "-r option used but no scontext Defined."
+echo "use --help for more information"
+error=1
+elif [ ! -z "$scon" ]; then
+echo "Error:"
+echo "-r option and -s can't used simultaneosly."
+echo "use --help for more information"
+error=1
+else
+echo "-r option used"
+echo "$rcon fixes will be ignored."
+echo ""
+fi
+fi
+
+if [ ! -z "$(echo "$opt" | grep -e '--clean')" ] && [ $error -eq 0 ]; then
 rm -rf sepolicy/vendor/*.te
+echo "-Cleaning Previous Sepolicy Fixes"
+echo ""
+fi
+
+for file in $@; do
+if [ -f $file ] && [ $error -eq 0 ]; then
+echo "-Selinux Fixes From $file"
+echo ""
+sepolicy $file
+rm -rf temp.txt
+echo ""
 fi
 done
-
-sepolicy $1
-rm -rf temp.txt
